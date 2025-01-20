b767a5d98713fa052717fbca7e4fc5cfec78a7b9
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2025-01-20 15:45:14 -0500

    typo in cf turnstile token

--- frontend/src/components/ConversationThread.tsx
+++ frontend/src/components/ConversationThread.tsx
@@ -115,7 +115,7 @@ const ConversationThread: React.FC<ConversationThreadProps> = ({
 						},
 						body: JSON.stringify({
 							//get token from local storage
-							token: localStorage.getItem('turnstile-token'),
+							token: localStorage.getItem('cf-turnstile-token'),
 							messages: [
 								{
 									role: 'user',
@@ -254,7 +254,7 @@ const ConversationThread: React.FC<ConversationThreadProps> = ({
 						'Content-Type': 'application/json',
 					},
 					body: JSON.stringify({
-						token: localStorage.getItem('turnstile-token'),
+						token: localStorage.getItem('cf-turnstile-token'),
 						messages,
 					}),
 					signal: controller.signal,
